033544fcf3b95bce08210305acea97a5844c2921
Kazeem Amuzat Asojuomode <77699947+amuzatk@users.noreply.github.com>
2023-10-10 02:10:10 +0100

    search and pag done

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,17 +1,111 @@
+// // // components/PostList.tsx
+// import React, { useEffect, useState } from "react";
+// import { usePosts, fetchPosts } from "../api/posts";
+// import { BlogPost } from "../interfaces";
+// import PostCard from "./PostCard";
+// import styled from "styled-components";
+// import { useDispatch } from "react-redux";
+// import { setPosts } from "../redux/reducers/posts";
+// import { Pagination } from "antd";
+
+//   const PostList: React.FC<{ searchValue: string }> = ({ searchValue }) => {
+//   const { data, isLoading, isError } = usePosts();
+//   const dispatch = useDispatch();
+
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const itemsPerPage = 10;
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const posts = await fetchPosts();
+//         const serializedPosts = posts.map((post: any) => ({
+//           ...post,
+//           created_at: new Date().toISOString(),
+//           updated_at: new Date().toISOString(),
+//         }));
+//         dispatch(setPosts(serializedPosts));
+//       } catch (error) {
+//         console.error("An error occurred while fetching posts:", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, [dispatch]);
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (isError) {
+//     return <div>Error fetching posts.</div>;
+//   }
+
+//   // Check if data is available and not undefined
+//   if (!data) {
+//     return null;
+//   }
+
+//   // Now you can safely slice the data
+//   const startIndex = (currentPage - 1) * itemsPerPage;
+//   const endIndex = startIndex + itemsPerPage;
+//   const displayedData = data.slice(startIndex, endIndex);
+
+//   const handlePageChange = (page) => {
+//     setCurrentPage(page);
+//   };
+
+//   return (
+//     <>
+//       <h1>Blog Posts</h1>
+//       <StyledCont>
+//         {displayedData.map((post: BlogPost) => (
+//           <div key={post.id}>
+//             <PostCard post={post} />
+//           </div>
+//         ))}
+//       </StyledCont>
+//       <Pagination
+//         current={currentPage}
+//         total={data.length}
+//         pageSize={itemsPerPage}
+//         onChange={handlePageChange}
+//       />
+//     </>
+//   );
+// };
+
+// export default PostList;
+// const StyledCont = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-wrap: wrap;
+//   flex-grow: 1;
+//   flex-basis: auto;
+//   gap: 15px 10px;
+// `;
+
+
 // components/PostList.tsx
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { usePosts, fetchPosts } from "../api/posts";
 import { BlogPost } from "../interfaces";
 import PostCard from "./PostCard";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { setPosts } from "../redux/reducers/posts";
-import Button from "./Button/Button";
+import { Pagination } from "antd";
 
-// const PostList: React.FC = () => {
-  const PostList: React.FC<{ searchValue: string }> = ({ searchValue }) => {
+const PostList: React.FC<{ searchValue: string; currentPage: number; onPageChange: (page: number) => void }> = ({
+  searchValue,
+  currentPage,
+  onPageChange,
+}) => {
   const { data, isLoading, isError } = usePosts();
   const dispatch = useDispatch();
+
+  const [itemsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,8 +113,8 @@ import Button from "./Button/Button";
         const posts = await fetchPosts();
         const serializedPosts = posts.map((post: any) => ({
           ...post,
-          created_at: new Date().toISOString(), // Convert to ISO string
-          updated_at: new Date().toISOString(), // Convert to ISO string
+          created_at: new Date().toISOString(),
+          updated_at: new Date().toISOString(),
         }));
         dispatch(setPosts(serializedPosts));
       } catch (error) {
@@ -39,41 +133,33 @@ import Button from "./Button/Button";
     return <div>Error fetching posts.</div>;
   }
 
-  if (!data) {
-    return null;
-  }
+  // Filter data based on searchValue in the post title
+  const filteredData = data.filter((post: BlogPost) =>
+    post.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
-
-  const filteredData = data.filter((post: BlogPost) => {
-    // Filter posts based on the search input value
-    return post.title.toLowerCase().includes(searchValue.toLowerCase()) || 
-           post.body.toLowerCase().includes(searchValue.toLowerCase());
-  });
+  // Calculate the startIndex and endIndex based on currentPage and itemsPerPage
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedData = filteredData.slice(startIndex, endIndex);
 
   return (
-    // <>
-    //   <h1>Blog Posts</h1>
-    //   <StyledCont>
-    //     {data.map((post: BlogPost) => (
-    //       <div key={post.id}>
-    //         <PostCard post={post} />
-    //       </div>
-    //     ))}
-    //   </StyledCont>
-    // </>
-
-
     <>
-    <h1>Blog Posts</h1>
-    {/* ... */}
-    <StyledCont>
-      {filteredData.map((post: BlogPost) => (
-        <div key={post.id}>
-          <PostCard post={post} />
-        </div>
-      ))}
-    </StyledCont>
-  </>
+      <h1>Blog Posts</h1>
+      <StyledCont>
+        {displayedData.map((post: BlogPost) => (
+          <div key={post.id}>
+            <PostCard post={post} />
+          </div>
+        ))}
+      </StyledCont>
+      <Pagination
+        current={currentPage}
+        total={filteredData.length}
+        pageSize={itemsPerPage}
+        onChange={onPageChange} // Pass the onPageChange callback
+      />
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,7 @@
-// // pages/index.tsx
 // import React, { useState } from "react";
 // import CreatePostModal from "../components/CreatePostModal";
 // import PostList from '../components/PostList';
-// import { Modal, Button } from "antd"; // Import Ant Design components
-// import CreatePost from "../components/CreatePost";
-
-// const Home: React.FC = () => {
-//   const [isModalVisible, setIsModalVisible] = useState(false);
-
-//   const showModal = () => {
-//     setIsModalVisible(true);
-//   };
-
-//   const handleOk = () => {
-//     // Handle form submission here (e.g., dispatching a createPost action)
-//     setIsModalVisible(false);
-//   };
-
-//   const handleCancel = () => {
-//     setIsModalVisible(false);
-//   };
-//   return (
-//     <div>
-//        <Button type="primary" onClick={showModal}>
-//         Create Post
-//       </Button>
-//       <Modal
-//         title="Create a New Post"
-//         open={isModalVisible}
-//         onOk={handleOk}
-//         onCancel={handleCancel}
-//       >
-//         <CreatePost />
-//       </Modal>
-
-//       <PostList />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-// import React, { useState } from "react";
-// import CreatePostModal from "../components/CreatePostModal";
-// import PostList from '../components/PostList';
-// import { Modal, Button } from "antd"; // Import Ant Design components
+// import { Modal, Button, Input } from "antd"; // Import Ant Design components
 // import CreatePost from "../components/CreatePost";
 
 // const Home: React.FC = () => {
@@ -69,50 +25,57 @@
 //     setSearchValue(e.target.value);
 //   };
 
+//   const handleSearch = () => {
+//     // You can trigger the search here, e.g., by passing the searchValue to the PostList component.
+//     // This depends on how you want to implement the search logic.
+//     // For now, let's just print the searchValue to the console.
+//     console.log("Search Value:", searchValue);
+//   };
+
 //   return (
 //     <div>
 //       <Button type="primary" onClick={showModal}>
 //         Create Post
 //       </Button>
-//       <input
+//       <Input
 //         type="text"
 //         placeholder="Search posts..."
 //         value={searchValue}
 //         onChange={handleSearchChange}
 //       />
+//       <Button onClick={handleSearch}>Search</Button>
 //       <Modal
 //         title="Create a New Post"
-//         open={isModalVisible}
+//         visible={isModalVisible} // Use "visible" instead of "open"
 //         onOk={handleOk}
 //         onCancel={handleCancel}
 //       >
 //         <CreatePost />
 //       </Modal>
 
-//       <PostList searchValue={searchValue} /> {/* Pass the searchValue as a prop */}
+//       <PostList searchValue={searchValue} />
 //     </div>
 //   );
 // };
 
 // export default Home;
 
-
 import React, { useState } from "react";
 import CreatePostModal from "../components/CreatePostModal";
-import PostList from '../components/PostList';
+import PostList from "../components/PostList";
 import { Modal, Button, Input } from "antd"; // Import Ant Design components
 import CreatePost from "../components/CreatePost";
 
 const Home: React.FC = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [searchValue, setSearchValue] = useState(""); // Add state for search input value
+  const [searchValue, setSearchValue] = useState("");
+  const [currentPage, setCurrentPage] = useState(1); // Add currentPage state
 
   const showModal = () => {
     setIsModalVisible(true);
   };
 
   const handleOk = () => {
-    // Handle form submission here (e.g., dispatching a createPost action)
     setIsModalVisible(false);
   };
 
@@ -125,10 +88,13 @@ const Home: React.FC = () => {
   };
 
   const handleSearch = () => {
-    // You can trigger the search here, e.g., by passing the searchValue to the PostList component.
-    // This depends on how you want to implement the search logic.
-    // For now, let's just print the searchValue to the console.
-    console.log("Search Value:", searchValue);
+    // When searching, reset currentPage to 1
+    setCurrentPage(1);
+  };
+
+  const handlePageChange = (page: number) => {
+    // Update the currentPage state when the user interacts with pagination
+    setCurrentPage(page);
   };
 
   return (
@@ -145,16 +111,81 @@ const Home: React.FC = () => {
       <Button onClick={handleSearch}>Search</Button>
       <Modal
         title="Create a New Post"
-        visible={isModalVisible} // Use "visible" instead of "open"
+        visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
       >
         <CreatePost />
       </Modal>
 
-      <PostList searchValue={searchValue} />
+      <PostList currentPage={currentPage} searchValue={searchValue} onPageChange={handlePageChange} />
     </div>
   );
 };
 
 export default Home;
+
+
+
+// import React, { useState } from "react";
+// import CreatePostModal from "../components/CreatePostModal";
+// import PostList from "../components/PostList";
+// import { Modal, Button, Input } from "antd"; // Import Ant Design components
+// import CreatePost from "../components/CreatePost";
+
+// const Home: React.FC = () => {
+//   const [isModalVisible, setIsModalVisible] = useState(false);
+//   const [searchValue, setSearchValue] = useState(""); // Add state for search input value
+
+//   const showModal = () => {
+//     setIsModalVisible(true);
+//   };
+
+//   const handleOk = () => {
+//     // Handle form submission here (e.g., dispatching a createPost action)
+//     setIsModalVisible(false);
+//   };
+
+//   const handleCancel = () => {
+//     setIsModalVisible(false);
+//   };
+
+//   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     setSearchValue(e.target.value);
+//   };
+
+//   const handleSearch = () => {
+//     // You can trigger the search here by setting the searchValue state,
+//     // which will be passed to the PostList component as a prop.
+//     // The PostList component can then use this value to filter the data.
+//     console.log("Search Value:", searchValue);
+//   };
+
+//   return (
+//     <div>
+//       <Button type="primary" onClick={showModal}>
+//         Create Post
+//       </Button>
+//       <Input
+//         type="text"
+//         placeholder="Search posts..."
+//         value={searchValue}
+//         onChange={handleSearchChange}
+//       />
+//       <Button onClick={handleSearch}>Search</Button>
+//       <Modal
+//         title="Create a New Post"
+//         visible={isModalVisible} // Use "visible" instead of "open"
+//         onOk={handleOk}
+//         onCancel={handleCancel}
+//       >
+//         <CreatePost />
+//       </Modal>
+
+//       {/* Pass the searchValue as a prop to the PostList component */}
+//       <PostList searchValue={searchValue} />
+//     </div>
+//   );
+// };
+
+// export default Home;
